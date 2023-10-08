045b5fcdf77157651e9e7515f203cc4bd91bf654
Federico Corallini <fedecorallini@utn.edu.ar>
2023-10-08 17:41:47 -0300

    Primer commit: inicio de proyecto springboot

---
+++ adquisiciones/src/main/java/com/giuct/adquisiciones/AdquisicionesApplication.java
@@ -0,0 +1,13 @@
+package com.giuct.adquisiciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdquisicionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdquisicionesApplication.class, args);
+	}
+
+}
---
+++ adquisiciones/src/test/java/com/giuct/adquisiciones/AdquisicionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giuct.adquisiciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdquisicionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
